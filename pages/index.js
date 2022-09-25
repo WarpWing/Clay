e761b8b38d2e1b712ba494a8fb865a9d8c3c65a5
Ty Chermsirivatana <professornitro@gmail.com>
2022-09-25 17:33:34 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Is SkyCrypt Up?" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         Go check <a href="https://sky.shiiyu.moe/">here</a> dummy
         </p>
       </main>
 
