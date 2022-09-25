a54a118ea4526ff435c03900da2f37829eb68484
Ty Chermsirivatana <professornitro@gmail.com>
2022-09-25 17:42:25 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Not made by Nate Stringham <img src="https://media-exp1.licdn.com/dms/image/D5635AQHvdeQTlHbFbw/profile-framedphoto-shrink_200_200/0/1660266091311?e=1664748000&v=beta&t=83MggqTfb21ZN8e45kLzNOSrfvJuU_8n779qlhB-0QU" alt="Netlify Logo" className={styles.logo} /> for you
+        Not made by Nate Stringham <img src="https://media-exp1.licdn.com/dms/image/D5635AQHvdeQTlHbFbw/profile-framedphoto-shrink_200_200/0/1660266091311?e=1664748000&v=beta&t=83MggqTfb21ZN8e45kLzNOSrfvJuU_8n779qlhB-0QU" width="800" height="800" alt="Netlify Logo" className={styles.logo} /> for you
       </footer>
     </>
   )
